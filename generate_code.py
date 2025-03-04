34714882cb29a4b0b9e6ddc11e4fcbb8e3a15549
valentio <valentiocoding@gmail.com>
2025-03-04 11:21:48 +0700

    Update generate_code.py

--- generate_code.py
+++ generate_code.py
@@ -39,10 +39,13 @@ def input_data(generate_code, generate_desc, sequence_number):
     st.session_state.master = get_data("Master")
     if generate_code in st.session_state.master['ItemCode'].unique():
         st.error("Code already exists. Please try again.")
+        st.stop()
     if generate_desc in st.session_state.master['ItemName'].unique():
         st.error("Description already exists. Please try again.")
+        st.stop()
     if sequence_number in st.session_state.master['SequenceNumber'].unique():
         st.error("Sequence Number already exists. Please try again.")
+        st.stop()
     sheet = client.open_by_key(spreadsheet_id).worksheet("Master")
     sheet.append_row([generate_code, generate_desc, sequence_number], value_input_option="USER_ENTERED")
 
